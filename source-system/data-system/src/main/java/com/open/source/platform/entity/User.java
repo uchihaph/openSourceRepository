6213d16ec771057de5b8d445edad4ca1df771a06
hanguo <han.guo112@iwhalecloud.com>
2022-01-14 11:22:38 +0800

    项目前期调试

---
+++ source-system/data-system/src/main/java/com/open/source/platform/entity/User.java
@@ -0,0 +1,108 @@
+package com.open.source.platform.entity;
+
+
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hanguo
+ * @since 2022-01-13
+ */
+@TableName(value = "user")
+public class User extends Model<User> {
+
+    private static final long serialVersionUID = 1L;
+
+	@TableId(value="id", type= IdType.AUTO)
+	private Long id;
+	private String username;
+	private String password;
+	private String email;
+	private Integer telephone;
+	private String nick;
+	private String name;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(Integer telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	protected Serializable pkVal() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+			"id=" + id +
+			", username=" + username +
+			", password=" + password +
+			", email=" + email +
+			", telephone=" + telephone +
+			", nick=" + nick +
+			", name=" + name +
+			"}";
+	}
+}
---
+++ source-system/data-system/src/main/java/com/open/source/platform/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.open.source.platform.mapper;
+
+import com.open.source.platform.entity.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author hanguo
+ * @since 2022-01-13
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
