bec608592bff431415ddf1901bff141e99136879
korea <uchihaph@163.com>
2026-09-06 11:18:46 +0800

    更新

--- source-system/start-system/src/main/java/com/open/source/platform/OpenSourcePlatformApplication.java
+++ source-system/start-system/src/main/java/com/open/source/platform/OpenSourcePlatformApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,12 +16,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * @date 2021/12/27
  * @Description 启动类
  */
-//@SpringBootApplication(scanBasePackages = {"com.open.source.platform"})
-@SpringBootConfiguration
-@ComponentScan
+@SpringBootApplication(scanBasePackages = {"com.open.source.platform"})
 @MapperScan(basePackages = {"com.open.source.platform.mapper"})
 @EnableAsync
-@EnableAutoConfiguration
+
 public class OpenSourcePlatformApplication {
 
     public static void main(String[] args) {
